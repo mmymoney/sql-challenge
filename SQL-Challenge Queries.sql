9de4850d20d217a87e416cc86ee0958612d12520
Mike Mymoney <57593577+mmymoney@users.noreply.github.com>
2020-02-18 22:26:07 -0800

    Add files via upload
    * Create an image file of your ERD. -- QuickDBD-Free Diagram (2).png
    
    * Create a `.sql` file of your table schemata. -- Table Creation and Serial PK Creation.sql
    
    * Create a `.sql` file of your queries. -- SQL-Challenge Queries.sql

---
+++ SQL-Challenge Queries.sql
@@ -0,0 +1,66 @@
+-- Query 1: List the following details of each employee: employee number, last name, first name, gender, and salary.
+
+SELECT employees.emp_no, employees.last_name, employees.firstname, employees.gender, salaries.salary
+FROM employees
+JOIN salaries ON employees.emp_no = salaries.emp_no
+
+-- Query 2: List employees who were hired in 1986.
+
+SELECT * 
+FROM employees
+WHERE hire_date >= '1986-01-01'
+AND hire_date < '1987-01-01'
+
+-- Query 3: List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name, and start and end employment dates.
+
+SELECT departments.dept_no, departments.dept_name, employees.emp_no, employees.last_name, employees.firstname, department_managers.from_date, department_managers.to_date
+FROM department_managers
+JOIN employees ON employees.emp_no = department_managers.emp_no
+JOIN departments ON department_managers.dept_no = departments.dept_no
+
+-- Query 4: List the department of each employee with the following information: employee number, last name, first name, and department name.
+
+SELECT employees.emp_no, employees.last_name, employees.firstname, departments.dept_name
+FROM employees
+JOIN department_employees ON employees.emp_no = department_employees.emp_no
+JOIN departments ON department_employees.dept_no = departments.dept_no
+
+-- Query 5: List all employees whose first name is "Hercules" and last names begin with "B."
+
+SELECT *
+FROM employees
+
+-- Query 6: List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+-- Create View
+CREATE VIEW sales_dept AS
+	SELECT employees.emp_no, employees.last_name, employees.firstname, departments.dept_name
+	FROM employees
+	JOIN department_employees ON department_employees.emp_no = employees.emp_no
+	JOIN departments on department_employees.dept_no = departments.dept_no
+
+-- Select from View
+SELECT *
+FROM sales_dept
+WHERE dept_name = 'Sales'
+
+-- Query 7: List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+-- Create View
+CREATE VIEW sales_development_depts AS
+	SELECT employees.emp_no, employees.last_name, employees.firstname, departments.dept_name
+	FROM employees
+	JOIN department_employees ON department_employees.emp_no = employees.emp_no
+	JOIN departments on department_employees.dept_no = departments.dept_no
+
+-- Select from View
+SELECT *
+FROM sales_development_depts
+WHERE dept_name = 'Sales' or dept_name = 'Development'
+
+-- Query 8: In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+
+SELECT last_name, COUNT(last_name)
+FROM employees
+GROUP BY last_name
+ORDER BY count DESC
---
+++ Table Creation and Serial PK Creation.sql
@@ -0,0 +1,76 @@
+﻿--Initial Table Creation
+
+CREATE TABLE "departments" (
+    "dept_no" varchar   NOT NULL,
+    "dept_name" varchar   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "department_employees" (
+    "emp_no" varchar   NOT NULL,
+    "dept_no" varchar   NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL
+);
+
+CREATE TABLE "department_managers" (
+    "dept_no" varchar   NOT NULL,
+    "emp_no" varchar   NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL
+);
+
+CREATE TABLE "employees" (
+    "emp_no" varchar   NOT NULL,
+    "birth_date" date   NOT NULL,
+    "firstname" varchar   NOT NULL,
+    "last_name" varchar   NOT NULL,
+    "gender" varchar   NOT NULL,
+    "hire_date" date   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" varchar   NOT NULL,
+    "salary" float   NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL
+);
+
+CREATE TABLE "titles" (
+    "emp_no" varchar   NOT NULL,
+    "title" varchar   NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL
+);
+
+ALTER TABLE "department_employees" ADD CONSTRAINT "fk_department_employees_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "department_employees" ADD CONSTRAINT "fk_department_employees_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "department_managers" ADD CONSTRAINT "fk_department_managers_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "department_managers" ADD CONSTRAINT "fk_department_managers_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+
+-- Subsequent Primary Key Creation for Four tables
+
+ALTER TABLE department_employees ADD COLUMN id SERIAL PRIMARY KEY;
+ALTER TABLE department_managers ADD COLUMN id SERIAL PRIMARY KEY;
+ALTER TABLE salaries ADD COLUMN id SERIAL PRIMARY KEY;
+ALTER TABLE titles ADD COLUMN id SERIAL PRIMARY KEY;
+
